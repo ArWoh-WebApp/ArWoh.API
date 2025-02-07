4432df6196491a0eac75c350ca4bfef5bf4c54a7
phuctran362003 <trangiaphuc362003181@gmail.com>
2025-02-07 23:31:44 +0700

    init

---
+++ ArWoh.API/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        c.RoutePrefix = string.Empty; 
+    });
+}
+
+app.Run();
